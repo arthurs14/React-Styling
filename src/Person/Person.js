f44b915146d80baabc2d5687b4dd2c9fea55cbd1
Arthur Sandro <arthurcs426@gmail.com>
2020-10-21 15:51:59 -0700

    added starting files and components for styling

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Person.css';
+
+const Person = ({ click, name, age, changed }) => {
+  return (
+    <div className="Person">
+      <p onClick={click}>
+        I'm {name} and I am {age} years old!
+      </p>
+      <input type="text" onChange={changed} value={name} />
+    </div>
+  );
+};
+
+export default Person;
