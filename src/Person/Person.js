488aff0288cc139d5c3217822582915c97ad31a1
Arthur Sandro <arthurcs426@gmail.com>
2020-10-21 17:42:06 -0700

    added css modules to person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-
-  @media (min-width: 500px) {
-    .Person {
-      width: 450px;
-    }
-  }
-`;
+import classes from './Person.module.css';
 
 const Person = ({ click, name, age, changed }) => {
   return (
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={click}>
         {`I'm ${name} and I am ${age} years old!`}
       </p>
       <input type="text" onChange={changed} value={name} />
-    </StyledDiv>
+    </div>
   );
 };
 
